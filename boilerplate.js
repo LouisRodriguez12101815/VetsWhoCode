a0e50ec59dcd8444bf6ca812fa6a907068017757
louis rodriguez <125697808+LouisRodriguez12101815@users.noreply.github.com>
2024-04-14 14:36:03 -0400

    Update boilerplate.js
    I had not editted the default js code. Deleting the skills section of the site made the js file unneeded. Since the guidelines include the need to utilize html, css, and js I needed to utilize the file for something. I have been interested in what prompts pop up windows so figured it would be a good learning moment. I also added some variation to the greeting using the js file.

--- boilerplate.js
+++ boilerplate.js
@@ -1,5 +1,31 @@
-// Example: A simple JavaScript to toggle the visibility of the skills section
-document.getElementById('skills').addEventListener('click', function() {
-    this.querySelector('ul').style.display = this.querySelector('ul').style.display === 'none' ? 'block' : 'none';
-});
- 
+function showGreeting() {
+    var today = new Date();
+    var hour = today.getHours();
+    var greeting;
+
+    if (hour < 12) {
+        greeting = "Good Morning!";
+    } else if (hour < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+    document.getElementById("greeting").textContent = greeting;
+}
+
+window.onload = showGreeting;
+
+// Create an observer instance
+var observer = new IntersectionObserver(function(entries) {
+    // If the bottom element is visible in the viewport, show the popup
+    if (entries[0].isIntersecting) {
+        window.alert('You have scrolled to the bottom of the page!');
+        window.open('https://i.imgflip.com/541qur.jpg', '_blank');
+    }
+}, { threshold: 1.0 });  // 1.0 means that 100% of the element is visible
+
+// Select the element you want to observe
+var bottomElement = document.getElementById('bottom');
+
+// Start observing the element
+observer.observe(bottomElement);
